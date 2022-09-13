54813e433de736313b835086e53cf86fb3221e5c
unknown <philbranch243@gmail.com>
2022-09-13 17:35:22 -0500

    Nav tests passing

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -16,18 +16,18 @@ function Nav() {
     ];
     function categorySelected(name) {
         console.log(`${name} clicked`)
-      }
+    }
     return (
         <header>
             <h2>
-                <a href="/">
+                <a data-testid="link" href="/">
                     <span role="img" aria-label="camera"> 📸</span> Oh Snap!
                 </a>
             </h2>
             <nav>
                 <ul className="flex-row">
                     <li className="mx-2">
-                        <a href="#about">
+                        <a data-testid="about" href="#about">
                             About me
                         </a>
                     </li>
@@ -39,7 +39,7 @@ function Nav() {
                             className="mx-1"
                             key={category.name}
                         >
-                            <span onClick={()=> categorySelected(category.name)}>
+                            <span onClick={() => categorySelected(category.name)}>
                                 {category.name}
                             </span>
                         </li>
