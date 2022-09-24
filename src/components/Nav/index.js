888a305281a09aac3133334a2327de35c46c5fd7
unknown <philbranch243@gmail.com>
2022-09-24 13:14:59 -0500

    20.3.5

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { capitalizeFirstLetter } from '../../utils/helpers';
 
 function Nav(props) {
@@ -7,6 +7,9 @@ function Nav(props) {
     setCurrentCategory,
     currentCategory,
   } = props;
+  useEffect(() => {
+    document.title = capitalizeFirstLetter(currentCategory.name);
+  }, [currentCategory]);
 
   return (
     <header className="flex-row px-1">
@@ -27,8 +30,7 @@ function Nav(props) {
           </li>
           {categories.map((category) => (
             <li
-              className={`mx-1 ${
-                currentCategory.name === category.name && 'navActive'
+              className={`mx-1 ${currentCategory.name === category.name && 'navActive'
                 }`}
               key={category.name}
             >
