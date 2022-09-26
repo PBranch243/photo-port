a911edb54260a0814b812ce05dfaeb7ec8640418
unknown <philbranch243@gmail.com>
2022-09-26 09:16:53 -0500

    adding conditional rendering for contact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,11 +1,32 @@
 import React, { useState } from "react";
+import { validateEmail } from '../../utils/helpers';
 
 function ContactForm() {
+    const [errorMessage, setErrorMessage] = useState('');
     const [formState, setFormState] = useState({ name: '', email: '', message: '' });
     const { name, email, message } = formState;
 
     function handleChange(e) {
-        setFormState({ ...formState, [e.target.name]: e.target.value })
+        if (e.target.name === 'email') {
+            const isValid = validateEmail(e.target.value);
+            console.log(isValid);
+            // isValid conditional statement
+            if (!isValid) {
+                setErrorMessage('Your email is invalid.');
+                console.log('errorMessage', errorMessage);
+            } else {
+                if (!e.target.value.length) {
+                    setErrorMessage(`${e.target.name} is required.`);
+                    console.log('errorMessage', errorMessage);
+                } else {
+                    setErrorMessage('');
+                }
+            }
+        }
+        if (!errorMessage) {
+            setFormState({ ...formState, [e.target.name]: e.target.value });
+        }
+
     }
 
     function handleSubmit(e) {
@@ -29,6 +50,13 @@ function ContactForm() {
                     <label htmlFor="message">Message:</label>
                     <textarea name="message" defaultValue={message} onChange={handleChange} rows="5" />
                 </div>
+
+                {errorMessage && (
+                    <div>
+                        <p className="error-text">{errorMessage}</p>
+                    </div>
+                )}
+
                 <button type="submit">Submit</button>
             </form>
         </section>
